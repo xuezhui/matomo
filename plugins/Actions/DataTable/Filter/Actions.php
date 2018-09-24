6997e8d8da133061929bb25b9849eceedb8cd094
diosmosis <diosmosis@users.noreply.github.com>
2018-09-24 16:12:26 -0700

    URLs should not be decoded in Actions reports, they should be encoded for SafeDecodeLabel.

--- plugins/Actions/DataTable/Filter/Actions.php
+++ plugins/Actions/DataTable/Filter/Actions.php
@@ -51,7 +51,7 @@ class Actions extends BaseFilter
             foreach ($dataTable->getRows() as $row) {
                 $url = $row->getMetadata('url');
                 if ($url) {
-                    $row->setMetadata('segmentValue', urldecode($url));
+                    $row->setMetadata('segmentValue', urlencode($url));
                 }
 
                 // remove the default action name 'index' in the end of flattened urls and prepend $actionDelimiter
@@ -70,7 +70,7 @@ class Actions extends BaseFilter
 
         // TODO can we remove this one again?
         $table->queueFilter('GroupBy', array('label', function ($label) {
-            return urldecode($label);
+            return urlencode($label);
         }));
 
         foreach ($table->getRowsWithoutSummaryRow() as $row) {
